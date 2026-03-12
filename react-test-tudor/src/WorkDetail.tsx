bebb15b9d3cca7c6dea122a49fa2850a065f2120
222tudor <tudor.negrut2201@gmail.com>
2026-03-12 11:33:04 +0100

    calcolo ore in dettaglio

--- react-test-tudor/src/WorkDetail.tsx
+++ react-test-tudor/src/WorkDetail.tsx
@@ -14,6 +14,7 @@ export default function WorkDetail({ work, onClose, operators = [], tickets, cus
     const operator = operators.find((o: any) => o.id == work.operatorId)
     const ticket = tickets.find((t: any) => t.id == work.ticketId)
     const customer = customers.find((c: any) => c.id == ticket?.customerId)
+    const ore = (work.endDate - work.creationDate) / 3600000
 
     return (
         <div style={{ border: '1px solid #333', padding: '16px', marginTop: '16px' }}>
@@ -23,6 +24,7 @@ export default function WorkDetail({ work, onClose, operators = [], tickets, cus
             <p><strong>Operatore:</strong> {operator ? operator.name + ' ' + operator.surname : 'N/D'}</p>
             <p><strong>Cliente:</strong> {customer ? customer.name : 'N/D'}</p>
             <p><strong>Ticket:</strong> {ticket ? ticket.description : 'N/D'}</p>
+            <p><strong>Ore:</strong> {ore}</p>
         </div>
     )
 }
